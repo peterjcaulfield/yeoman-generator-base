33ea65a5d00f9879acaabdaba676fc9eefb31575
Peter Caulfield <peterjcaulfield@gmail.com>
2016-01-10 11:59:29 +0000

    add tests

---
+++ test/app.js
@@ -0,0 +1,21 @@
+var helpers = require('yeoman-test');
+var assert = require('yeoman-assert');
+var path = require('path');
+
+describe('base:app', function() {
+
+  before(function (done) {
+    helpers.run(path.join(__dirname, '../app'))
+      .withPrompts({ appname: 'foo' })
+      .on('end', done);
+  });  
+
+  it('generate a README file', function () {
+    assert.file('README.md');  
+  });
+
+  it('generates README using supplied app name from prompt', function () {
+    assert.fileContent('README.md', '# foo #');
+  });
+
+});
